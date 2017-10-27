e44f80998f68e56253b3ec5ba7ea5dab1025c702
Anand B Pillai <anandpillai@letterboxes.org>
2017-10-27 19:01:47 +0530

    => Python 3.0

--- matrix/multiply.py
+++ matrix/multiply.py
@@ -4,12 +4,12 @@ import sys
 def test(n=10):
     """ Demo multiplication of two square matrices of given dimension 'n' """
 
-    print 'Making two random square matrices of dimension',n,'...'
+    print('Making two random square matrices of dimension',n,'...')
     m1 = MatrixFactory.makeRandom(n, n)
     m2 = MatrixFactory.makeRandom(n, n)
 
     m3 = m1*m2
-    print m3
+    print(m3)
 
 if __name__ == "__main__":
     try:
